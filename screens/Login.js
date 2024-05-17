e6109827a19b27d1c6dabe0235846d14383ddac9
Nancy <rosalesnancy925@gmail.com>
2024-05-17 13:07:16 -0600

    add changes

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 import { BlurView } from 'expo-blur';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '../screens/firebase-config';
 import { useNavigation } from '@react-navigation/native';
-
-const uri = 'https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg';
-const profilePicture = 'https://export-download.canva.com/ITLns/DAGFdNITLns/2/0/0001-3790207295471102458.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20240517%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240517T003825Z&X-Amz-Expires=21908&X-Amz-Signature=2f118ec727c9cf4032c6e8ccd3f9d5de6649db1b886e70eec88a577b37fd01a7&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Dise%25C3%25B1o%2520sin%2520t%25C3%25ADtulo.png&response-expires=Fri%2C%2017%20May%202024%2006%3A43%3A33%20GMT';
+import googleImage from './go.png';
+import logo1 from './logo1.png';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const navigation = useNavigation();
 
   const app = initializeApp(firebaseConfig);
@@ -20,36 +21,83 @@ export default function Login() {
   const handleCreateAccount = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log('Account created!');
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch(error => {
-        console.log(error);
-        Alert.alert(error.message);
-      });
-  };
-
-  const handleSignIn = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        console.log('Signed in!');
+        console.log('Cuenta creada');
         const user = userCredential.user;
         console.log(user);
         navigation.navigate('Home');
       })
       .catch(error => {
         console.log(error);
-        Alert.alert(error.message);
+        Alert.alert('Error', 'Ha ocurrido un error. Por favor, inténtalo de nuevo más tarde.');
       });
+  };
+
+  const handleSignIn = () => {
+    setEmailError('');
+    setPasswordError('');
+
+    const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!email) {
+    setEmailError('Por favor, ingresa tu correo electrónico.');
+    return;
+  } else if (!email.match(emailFormat)) {
+    setEmailError('Por favor, ingresa un correo electrónico válido.');
+    return;
+  }
+
+  if (!password) {
+    setPasswordError('Por favor, ingresa tu contraseña.');
+    return;
+  }
+
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Registrado');
+        const user = userCredential.user;
+        console.log(user);
+        navigation.navigate('Home');
+      })
+      .catch(error => {
+        console.log(error);
+        if (error.code === 'auth/user-not-found') {
+      
+          Alert.alert(
+            'Usuario no encontrado',
+            'No hemos encontrado una cuenta con ese correo electrónico. ¿Quieres crear una nueva cuenta?',
+            [
+              {
+                text: 'Sí',
+                onPress: handleCreateAccount
+              },
+              {
+                text: 'No',
+                style: 'cancel'
+              }
+            ]
+          );
+        } else {
+          Alert.alert('Usuario no encontrado', 'No hemos encontrado una cuenta con ese correo electrónico. ¿Quieres crear una nueva cuenta?');
+        }
+      });
+  };
+
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log(user);
+      navigation.navigate('Home');
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Usuario no encontrado', 'No hemos encontrado una cuenta con ese correo electrónico. ¿Quieres crear una nueva cuenta?');
+    }
   };
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
-      <View style={{ width: 100, height: 100, backgroundColor: 'purple', position: 'absolute' }}></View>
-      <View style={{ width: 100, height: 100, backgroundColor: 'blue', top: 120, position: 'absolute', transform: [{ rotate: '25deg' }] }}></View>
-      <View style={{ width: 100, height: 100, backgroundColor: 'red', bottom: 120, position: 'absolute', borderRadius: 50, transform: [{ rotate: '50deg' }] }}></View>
       <ScrollView contentContainerStyle={{
         flex: 1,
         width: '100%',
@@ -59,21 +107,43 @@ export default function Login() {
       }}>
         <BlurView intensity={100}>
           <View style={styles.login}>
-            <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
             <View>
-              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>E-mail</Text>
-              <TextInput onChangeText={(text) => setEmail(text)} style={styles.input} placeholder="usuario@gmail.com" />
+              <Text style={{ fontSize: 15, fontWeight: '400', color: '#6C3895', textAlign: 'center' }}>Ingresa tu correo electrónico</Text>
+              <TextInput
+                onChangeText={(text) => setEmail(text)}
+                style={styles.input}
+                placeholder="Correo electrónico"
+                placeholderTextColor="#C7A5E2"
+              />
+              {emailError ? <Text style={styles.errorText}>{emailError}</Text> : null}
             </View>
             <View>
-              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>Password</Text>
-              <TextInput onChangeText={(text) => setPassword(text)} style={styles.input} placeholder="password" secureTextEntry={true} />
+              <Text style={{ fontSize: 15, fontWeight: '400', color: '#6C3895', textAlign: 'center' }}>Ingresa tu contraseña</Text>
+              <TextInput
+                onChangeText={(text) => setPassword(text)}
+                style={styles.input}
+                placeholder="Contraseña"
+                secureTextEntry={true}
+                placeholderTextColor="#C7A5E2"
+              />
+              {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
             </View>
-            <TouchableOpacity onPress={handleSignIn} style={[styles.button, { backgroundColor: '#00CFEB90' }]}>
-              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>Login</Text>
+            <TouchableOpacity onPress={handleSignIn} style={[styles.button, { backgroundColor: '#6C3895' }]}>
+              <Text style={{ fontSize: 15, fontWeight: '400', color: 'white' }}>Continuar</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={handleCreateAccount} style={[styles.button, { backgroundColor: '#6792F090' }]}>
-              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>Create Account</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity onPress={handleCreateAccount} style={[styles.button, {}]}>
+                <Text style={{ fontSize: 15, fontWeight: '400', color: '#6C3895', textDecorationLine: 'underline' }}>Crear cuenta</Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={{ fontSize: 15, fontWeight: '400', color: '#6C3895', textAlign: 'center', marginBottom: 30 }}>___________________ o ___________________ </Text>
+
+            <TouchableOpacity onPress={handleGoogleSignIn}>
+              <Image source={googleImage} style={{ width: 70, height: 70, marginTop: 10, }} />
             </TouchableOpacity>
+            <View style={styles.logoContainer}>
+              <Image source={logo1} style={{ width: 75, height: 100 }} />
+            </View>
           </View>
         </BlurView>
       </ScrollView>
@@ -82,54 +152,59 @@ export default function Login() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    image: {
-      width: '100%',
-      height: '100%',
-      resizeMode: 'cover',
-    },
-    login: {
-      width: 350,
-      height: 500,
-      borderColor: '#fff',
-      borderWidth: 2,
-      borderRadius: 10,
-      padding: 10,
-      alignItems: 'center',
-    },
-    profilePicture: {
-      width: 100,
-      height: 100,
-      borderRadius: 50,
-      borderColor: '#fff',
-      borderWidth: 1,
-      marginVertical: 30
-    },
-    input: {
-      width: 250,
-      height: 40,
-      borderColor: '#fff',
-      borderWidth: 2,
-      borderRadius: 10,
-      padding: 10,
-      marginVertical: 10,
-      backgroundColor: '#ffffff90',
-      marginBottom: 20
-    },
-    button: {
-      width: 250,
-      height: 40,
-      borderRadius: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginVertical: 10,
-      borderColor: '#fff',
-      borderWidth: 1,
-    }
-  
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  login: {
+    width: 350,
+    height: 500,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 90
+
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: '#6C3895',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 40,
+    marginTop: 35,
+    color: '#C7A5E2',
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  button: {
+    width: 300,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    backgroundColor: '#ffffff',
+  },
+  logoContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 40,
+    marginBottom: 60,
+  },
+  errorText: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: 'red',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+});
